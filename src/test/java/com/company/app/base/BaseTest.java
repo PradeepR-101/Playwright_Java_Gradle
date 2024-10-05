e19ecd9cf60045664acb4b05b6cbf31a01f41fef
PradeepRaj@1 <rpradee666@gmail.com>
2024-10-05 21:10:40 +0530

    sample added basetest and one testcase

--- src/test/java/com/company/app/base/BaseTest.java
+++ src/test/java/com/company/app/base/BaseTest.java
@@ -1,6 +1,171 @@
 package com.company.app.base;
 
-public class BaseTest {
+import com.microsoft.playwright.*;
+import io.qameta.allure.Allure;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.test.context.testng.AbstractTestNGSpringContextTests;
+import org.testng.ITestContext;
+import org.testng.ITestResult;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.BeforeSuite;
 
+import org.testng.log4testng.Logger;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.nio.file.Paths;
+import java.util.Properties;
+
+
+@Configuration
+public class BaseTest extends AbstractTestNGSpringContextTests {
+
+    private static final Logger logger = Logger.getLogger(BaseTest.class);
+
+    private static ThreadLocal<Playwright> playwrightThreadLocal = ThreadLocal.withInitial(Playwright::create);
+
+    private static ThreadLocal<Browser> browserThreadLocal = new ThreadLocal<>();
+
+    private static ThreadLocal<BrowserContext> browserContextThreadLocal = new ThreadLocal<>();
+
+    private static ThreadLocal<Page> pageThreadLocal = new ThreadLocal<>();
+
+    public static synchronized Playwright getPlaywright(){
+        return playwrightThreadLocal.get();
+    }
+
+    public static Browser getBrowser(){
+        return browserThreadLocal.get();
+    }
+
+    public static BrowserContext getBrowserContext(){
+        return browserContextThreadLocal.get();
+    }
+
+    public static Page getPage(){
+        return pageThreadLocal.get();
+    }
+
+    private String Headless;
+    public static Properties prop;
+    public static long start = System.currentTimeMillis();
+    public static ITestContext cont;
+
+    public void setup(){
+        logger.info("start time in milliseconds = " + start);
+        try{
+            Headless = prop.getProperty("headless").toLowerCase();
+            boolean head = Boolean.parseBoolean(Headless);
+            Browser browser = null;
+            switch (prop.getProperty("browser").toLowerCase()){
+                case "chromium":
+                    browser = (getPlaywright().chromium().launch(new BrowserType.LaunchOptions().setHeadless(head).setChannel("chromium")));
+                    break;
+                case "chrome":
+                    browser = getPlaywright().chromium().launch(new BrowserType.LaunchOptions().setHeadless(head).setChannel("chrome"));
+                    break;
+                case "msedge":
+                    browser = getPlaywright().chromium().launch(new BrowserType.LaunchOptions().setHeadless(head).setChannel("msedge"));
+                    break;
+                case "firefox":
+                    browser = getPlaywright().firefox().launch(new BrowserType.LaunchOptions().setHeadless(head).setChannel("firefox"));
+                    break;
+                case "safari":
+                    browser = getPlaywright().webkit().launch(new BrowserType.LaunchOptions().setHeadless(head).setChannel("safari"));
+                    break;
+                case "mobile":
+                    browser = getPlaywright().chromium().launch(new BrowserType.LaunchOptions().setHeadless(head).setChannel("chromium"));
+                    break;
+                default:
+                    logger.info("please pass browser name...........");
+                    break;
+            }
+            browserThreadLocal.set(browser);
+            //browserContextThreadLocal.set(browserThreadLocal.get().newContext(/*new Browser.NewContextOptions().setRecordVideoDir(Paths.get("videos/"))*/));
+            //browserContextThreadLocal.set(browserThreadLocal.get().newContext(new Browser.NewContextOptions().setIgnoreHTTPSErrors(true)));
+
+            browserContextThreadLocal.set(browserThreadLocal.get().newContext(new Browser.NewContextOptions().setStorageStatePath(Paths.get("SessionStorage.json"))));
+
+            //browserContextThreadLocal.get().storageState(new BrowserContext.StorageStateOptions().setPath(Paths.get("SessionStorage.json")));
+
+            browserContextThreadLocal.get().tracing().start(new Tracing.StartOptions()
+                    .setScreenshots(true)
+                    .setSnapshots(true)
+                    .setSources(true));
+            pageThreadLocal.set(browserContextThreadLocal.get().newPage());
+        }catch(Exception e){
+            logger.error("" + Long.toString(Thread.currentThread().getId()) + "-Exception occured in setup method- "+ e.getMessage());
+            Allure.label("Exception","setup method - "+e.getLocalizedMessage());
+            closeThreads();
+        }
+
+    }
+
+    public void tearDown(ITestResult iTestResult) throws IOException{
+        try{
+            if(iTestResult.getStatus() == ITestResult.SUCCESS || iTestResult.getStatus() == ITestResult.SKIP){
+                getBrowserContext().tracing().stop();
+            } else{
+                getBrowserContext().tracing().stop(new Tracing.StopOptions()
+                        .setPath(Paths.get("traces/" + iTestResult.getMethod().getMethodName().replace("()","") + ".zip")));
+            }
+        } catch (Exception e){
+        }
+        //closeThreads();
+        try{
+            if(pageThreadLocal.get()!=null){
+                pageThreadLocal.get().close();
+                pageThreadLocal.remove();
+            }
+        } catch(Exception e){
+        }
+    }
+
+    @BeforeSuite(groups = {"QA","PROD","Regression","E2E","TEST"})
+    public void load(){
+        prop = new Properties();
+        String Env = System.getProperty("env");
+        try{
+            prop.load(new FileInputStream("./src/main/resources/application-QA.properties"));
+            //prop.load(new FileInputStream("./src/main/resources/application-"+Env+".properties"));
+        } catch (Exception e){
+            throw new RuntimeException(e);
+        }
+    }
+
+    @BeforeMethod(groups = {"QA","PROD","Regression","E2E","TEST"})
+    public void tearup(){
+        setup();
+    }
+
+    @AfterMethod(groups = {"QA","PROD","Regression","E2E","TEST"})
+    public void teardown(ITestResult iTestResult) throws IOException {
+        tearDown(iTestResult);
+    }
+
+    @AfterSuite(groups = {"QA","PROD","Regression","E2E","TEST"})
+    public void afterSuite(){
+    }
+
+    public void closeThreads(){
+        try{
+            getBrowserContext().tracing().stop();
+        } catch (Exception e){
+        }
+        if(pageThreadLocal.get()!=null){
+            pageThreadLocal.get().close();
+            pageThreadLocal.remove();
+        }
+        if(browserThreadLocal.get().isConnected() || browserThreadLocal.get()!=null){
+            browserThreadLocal.get().close();
+            browserThreadLocal.remove();
+        }
+        if(playwrightThreadLocal.get()!=null){
+            playwrightThreadLocal.get().close();
+            playwrightThreadLocal.remove();
+        }
+    }
 
 }
--- src/test/java/com/company/app/tests/pagetests/LoginPageTest.java
+++ src/test/java/com/company/app/tests/pagetests/LoginPageTest.java
@@ -1,4 +1,96 @@
 package com.company.app.tests.pagetests;
 
-public class LoginPageTest {
+import com.company.app.base.BaseTest;
+import com.company.app.listeners.AllureListeners;
+import com.company.app.pages.LoginPage;
+import com.company.app.utils.CommonUtils;
+import com.microsoft.playwright.*;
+import io.qameta.allure.Allure;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Description;
+import org.testng.ITestResult;
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+import org.testng.asserts.SoftAssert;
+
+import java.util.*;
+
+//import static com.company.app.utils.CommonUtils.getExceptionType;
+//import static com.company.app.utils.GeneralUtils.AllureAddScreenshot;
+
+@SpringBootTest
+public class LoginPageTest extends BaseTest {
+
+    private static final Logger logger = LoggerFactory.getLogger(LoginPageTest.class);
+
+//    @Autowired
+//    public CommonUtils commonUtils;
+    public LoginPage loginPage;
+
+    @Value("${appurl}")
+    private String appurl;
+
+    @Value("${environment}")
+    private String environment;
+
+
+    @Description("Verify sample test")
+    @Test(groups = {"Regression"})
+    public void verify_sample_test() {
+
+        Playwright playwright = Playwright.create();
+        Browser browser = playwright.chromium().launch(new BrowserType.LaunchOptions().setHeadless(false));
+
+        BrowserContext browserContext = browser.newContext();
+        Page page = browserContext.newPage();
+        page.navigate("https://www.google.com");
+        System.out.println(page.title());
+
+        BrowserContext browserContext1 = browser.newContext();
+        Page page1 = browserContext1.newPage();
+        page1.navigate("https://www.naveenautomationlabs.com");
+        System.out.println(page1.title());
+    }
+
+    @Description("Verify login test")
+    @Test(groups = {"Regression"})
+    public void verify_Login_Test(){
+
+        Allure.label("testsuite",cont.getCurrentXmlTest().getSuite().getName());
+        Allure.label("testingType","uitesting");
+
+        SoftAssert softAssert = new SoftAssert();
+        ITestResult result = Reporter.getCurrentTestResult();
+        String manNo = "";
+
+        try{
+            getPage().navigate(appurl);
+
+            Allure.label("application","default-application");
+            softAssert.assertAll();
+        } catch (PlaywrightException p){
+//            String exceptionName = getExceptionType(p);
+//            if(!exceptionName.equalsIgnoreCase("DriverException")){
+//                AllureAddScreenshot(getPage());
+//            }
+//            Allure.label("application_error",exceptionName);
+//            throw p;
+        } catch (Exception e){
+//            AllureAddScreenshot(getPage());
+            logger.error("Exception occured in Login method" + e.getMessage());
+//            Allure.label("application_error",getExceptionType(e));
+//            throw e;
+        } finally {
+//            if(environment.equalIgnoreCase("QA")){
+//                //
+//            }
+//            AllureListeners.orderNumber.put(result.getMethod().getMethodName(), orderNo);
+        }
+
+
+    }
 }
